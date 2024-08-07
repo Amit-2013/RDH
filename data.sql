b3ab41a38786a9db4a754c3b8dc7ed803c6af0ce
Amit-2013 <amit.malhotra.1982@gmail.com>
2024-08-07 22:14:16 +0530

    Delete data.sql e lol lol

--- data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Vehicle (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    customer_name TEXT NOT NULL,
-    mobile_number TEXT NOT NULL,
-    vin_number TEXT UNIQUE NOT NULL,
-    registration_number TEXT UNIQUE NOT NULL,
-    mileage INTEGER NOT NULL
-);
